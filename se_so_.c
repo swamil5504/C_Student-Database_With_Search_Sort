7851b489276ceb0af04a43520c339184f108e747
Swamil Randive <111211405+swamil5504@users.noreply.github.com>
2023-09-12 22:31:19 +0530

    Add files via upload

---
+++ se_so_.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student {
+    char name[50];
+    int rollNo;
+    float marks;
+};
+
+void accept(struct student students[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Enter Student details:\n");
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        printf("Roll No: ");
+        scanf("%d", &students[i].rollNo);
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+    }
+}
+
+void display(struct student students[], int n) {
+    printf("Name\t\tRollNo\tMarks\n");
+    for (int i = 0; i < n; i++) {
+        printf("%s\t\t%d\t%.2f\n", students[i].name, students[i].rollNo, students[i].marks);
+    }
+}
+
+int linearSearch(struct student students[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (students[i].rollNo == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(struct student students[], int left, int right, int key) {
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (students[mid].rollNo == key) {
+            return mid;
+        } else if (students[mid].rollNo < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+void insertionSort(struct student students[], int n) {
+    for (int i = 1; i < n; i++) {
+        struct student temp = students[i];
+        int j = i - 1;
+        while (j >= 0 && students[j].rollNo > temp.rollNo) {
+            students[j + 1] = students[j];
+            j--;
+        }
+        students[j + 1] = temp;
+    }
+}
+
+void selectionSort(struct student students[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minpos = i;
+        for (int j = i + 1; j < n; j++) {
+            if (students[j].rollNo < students[minpos].rollNo) {
+                minpos = j;
+            }
+        }
+        if (minpos != i) {
+            struct student temp = students[i];
+            students[i] = students[minpos];
+            students[minpos] = temp;
+        }
+    }
+}
+
+void shellSort(struct student students[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            struct student temp = students[i];
+            int j;
+            for (j = i; j >= gap && students[j - gap].rollNo > temp.rollNo; j -= gap) {
+                students[j] = students[j - gap];
+            }
+            students[j] = temp;
+        }
+    }
+}
+
+int main() {
+    int n, choice, key, index;
+
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    struct student students[n];
+
+    accept(students, n);
+
+    printf("Select a sorting Technique\n");
+    printf("1. Insertion Sort\n");
+    printf("2. Selection Sort\n");
+    printf("3. Shell Sort\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            insertionSort(students, n);
+            break;
+        case 2:
+            selectionSort(students, n);
+            break;
+        case 3:
+            shellSort(students, n);
+            break;
+        default:
+        printf("Invalid choice.\n");
+            return 1;
+    }
+  
+    printf("\nSorted Student Data:\n");
+    display(students, n);
+
+    printf("Enter the Roll Number to search: ");
+    scanf("%d", &key);
+
+    index = linearSearch(students, n, key);
+
+    if (index != -1) {
+        printf("Student found!\n");
+        printf("Name: %s\n", students[index].name);
+        printf("Roll Number: %d\n", students[index].rollNo);
+        printf("Marks: %.2f\n", students[index].marks);
+    } else {
+        printf("Student not found.\n");
+    }
+
+    return 0;
+}
